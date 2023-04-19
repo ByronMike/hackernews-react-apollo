44169e05d89542e0340fca6756497d5fa1c9f565
ByronMike <auger.michaell@gmail.com>
2023-04-19 12:07:31 +0000

    Authentification - end

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
+// 8) Import the Apollo dependencies  for the token
+import { setContext } from '@apollo/client/link/context';
+// 10) Import the key we need to retrieve the token from localStorage
+import { AUTH_TOKEN } from './constants';
 
 // 1 We import all the dependencies we need to wire up the Apollo client, all from @apollo/client.
 import {
@@ -18,9 +22,24 @@ const httpLink = createHttpLink({
   uri: "http://localhost:4000",
 });
 
+// 7) Apollo provides a nice way for authenticating (transmettre le token) all requests by using the concept of middleware, implemented as an Apollo Link.
+// This middleware will be invoked every time ApolloClient sends a request to the server. Apollo Links allow us to create middlewares that modify requests before they are sent to the server.
+// Explanation :  first, we get the authentication token from localStorage if it exists; after that, we return the headers to the context so httpLink can read them.
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
 // 3 We instantiate ApolloClient by passing in the httpLink and a new instance of an InMemoryCache.
 const client = new ApolloClient({
-  link: httpLink,
+  //link: httpLink,
+  // 9) ApolloClient gets instantiated with the correct link -
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
